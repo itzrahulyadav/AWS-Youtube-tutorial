0b5516278df94c708a5c3064424569ea598a44da
Rahul Kumar Yadav <65400893+itzrahulyadav@users.noreply.github.com>
2025-02-16 23:33:59 +0530

    Create index.mjs

---
+++ Beginner/lambda/demo-application/index.mjs
@@ -0,0 +1,14 @@
+import * as fs from 'node:fs';
+
+const html = fs.readFileSync('index.html', { encoding: 'utf8' });
+
+export const handler = async (event) => {    
+    const response = {
+        statusCode: 200,
+        headers: {
+            'Content-Type': 'text/html',
+        },
+        body: html,
+    };
+    return response;
+};
