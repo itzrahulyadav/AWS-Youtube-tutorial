000e99d2a18b34fcbbbbb50968e6fe7a81786215
Rahul Yadav <rahulkyadav9691@gmail.com>
2025-02-17 19:46:40 +0000

    add new file

--- Advanced/Lambda/golang/main.go
+++ Advanced/Lambda/golang/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"net/http"
+    "io"
 )
 func welcome(w http.ResponseWriter, _ *http.Request) {
 	fmt.Fprintf(w,"Hello from Go!!")
@@ -10,8 +11,24 @@ func welcome(w http.ResponseWriter, _ *http.Request) {
 
 func main() {
 
-	http.HandleFunc("/", welcome)
-	if err := http.ListenAndServe(":8080", nil); err != nil {
+	// response,err := http.Get("https://pokeapi.co/api/v2/pokemon/ditto")
+	response,err := http.Get("https://official-joke-api.appspot.com/random_joke")
+	if err != nil {
 		fmt.Println(err)
 	}
-}l
+	fmt.Println(response.Status)
+
+
+	body, err := io.ReadAll(response.Body)
+	if err != nil {
+		fmt.Println("Error reading response body:", err)
+		return
+	}
+
+	fmt.Println("Response body:", string(body))
+	// http.Handle("/", http.FileServer(http.Dir("./")))
+	// http.HandleFunc("/welcome", welcome)
+	// if err := http.ListenAndServe(":8080", nil); err != nil {
+	// 	fmt.Println(err)
+	// }
+}
