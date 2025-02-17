a37969321454443745f96fb66ee319b2fdccc9e1
Rahul Yadav <rahulkyadav9691@gmail.com>
2025-02-17 17:55:09 +0000

    modified the file

--- Advanced/Lambda/Flask_app/app.py
+++ Advanced/Lambda/Flask_app/app.py
@@ -1,14 +1,15 @@
-from flask import Flask
+from flask import Flask,render_template
 app = Flask(__name__)
 
 @app.route('/')
-def main():
-    return "Hello from flask"
+@app.route('/')
+def index():
+    return render_template('index.html')
 
 @app.route('/home')
 def welcome():
-    return "Welcome to the home page"
-
+    return render_template('home.html') 
+   
 
 if __name__ == '__main__':
     app.run(debug=True)
